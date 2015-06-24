864734ca853929d260a9ec48f3bef69a367aa25b
Michael Cogswell <cogswell@vt.edu>
2015-06-24 02:09:46 -0400

    zeiler and guided backprop vis implemented; save reconstructions to directory

--- lib/reconstruct.py
+++ lib/reconstruct.py
@@ -1,5 +1,11 @@
+import os
+import os.path as pth
+import tempfile
+
 import numpy as np
 import matplotlib.pyplot as plt
+
+import google.protobuf.text_format as text_format
 
 import caffe
 import caffe.proto.caffe_pb2 as cpb
@@ -8,11 +14,34 @@ from cogswell import keyboard
 
 import config
 
-def reconstruct(spec, model_params):
-    net = caffe.Net(spec, model_params, caffe.TEST)
-    blobs = net.forward()
+def reconstruct(spec, model_params, layer_name, dirname, relu_type='DECONV'):
+    # load net spec and figure out which blob to use
+    net_param = cpb.NetParameter()
+    with open(spec, 'r') as f:
+        text_format.Merge(f.read(), net_param)
+    layers = dict(zip([l.name for l in net_param.layer], net_param.layer))
+    layer = layers[layer_name]
+    # don't support old net versions for now
+    assert len(net_param.layers) == 0
+    # for now, only accept layers with one top blob so I don't have to give
+    # the blob's name
+    assert len(layer.top) == 1
+    blob_name = layer.top[0]
 
-    blob = net.blobs['conv1']
+    # set relu backprop type and generate temp net
+    backprop_types = dict(zip(cpb.ReLUParameter.BackpropType.keys(),
+                              cpb.ReLUParameter.BackpropType.values()))
+    for layer in net_param.layer:
+        if layer.type == 'ReLU':
+            layer.relu_param.backprop_type = backprop_types[relu_type]
+    tmpspec = tempfile.NamedTemporaryFile()
+    tmpspec.write(text_format.MessageToString(net_param))
+    tmpspec.flush()
+
+    # initialize the net and forward()
+    net = caffe.Net(tmpspec.name, model_params, caffe.TEST)
+    blobs = net.forward()
+    blob = net.blobs[blob_name]
 
     num = blob.data.shape[0]
     max_idxs = []
@@ -20,8 +49,9 @@ def reconstruct(spec, model_params):
         flat_idx = blob.data[n].argmax()
         idx = np.unravel_index(flat_idx, blob.data.shape[1:])
         blob.diff[n][idx] = blob.data[n][idx]
+        max_idxs.append(idx)
 
-    net.backward(start='relu1', end='conv1')
+    net.backward(start=layer_name, end='conv1')
     mean = config.load_mean_image()[15:-14, 15:-14]
     imgs = net.blobs['data'].data.transpose([0, 2, 3, 1])
     recons = net.blobs['data'].diff.transpose([0, 2, 3, 1])
@@ -33,7 +63,8 @@ def reconstruct(spec, model_params):
 
     def show(i):
         img = showable(imgs[i])
-        reimg = showable(recons[i])
+        # TODO: this needs to be set somehow
+        reimg = showable(32*recons[i])
         plt.subplot(221)
         plt.imshow(img)
         plt.subplot(222)
@@ -42,6 +73,14 @@ def reconstruct(spec, model_params):
         patchimg = np.copy(img)
         patchimg[recons[i] != 0] = reimg[recons[i] != 0]
         plt.imshow(patchimg)
-        plt.show()
+        plt.title('filter idx: {}'.format(max_idxs[i]))
+        try:
+            os.mkdir(pth.join(dirname, layer_name))
+        except OSError:
+            pass
+        plt.savefig(pth.join(dirname, layer_name, 'recon_{}_{}_{}.jpg'.format(layer_name, relu_type, i)))
 
-    keyboard('hi')
+    for i in range(num):
+        show(i)
+
+    tmpspec.close()
--- scripts/test.py
+++ scripts/test.py
@@ -10,7 +10,21 @@ import config as config
 from reconstruct import *
 
 def main():
-    reconstruct('specs/train_val.prototxt', 'caffe/models/bvlc_reference_caffenet/bvlc_reference_caffenet.caffemodel')
+    '''
+    Usage:
+        vis.py <layer_name>
+    '''
+    import docopt, textwrap
+    main_args = docopt.docopt(textwrap.dedent(main.__doc__))
+
+    layer_name = main_args['<layer_name>']
+
+    reconstruct('specs/train_val.prototxt',
+                'caffe/models/bvlc_reference_caffenet/bvlc_reference_caffenet.caffemodel',
+                layer_name,
+                'results/',
+                # NORMAL, DECONV, GUIDED
+                relu_type='GUIDED')
 
 if __name__ == '__main__':
     main()
