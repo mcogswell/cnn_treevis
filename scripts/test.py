9d0b78813e06e6167e030bbad9d43bed1740d7ae
Michael Cogswell <cogswell@vt.edu>
2015-06-30 12:37:29 -0400

    test script takes any net

--- scripts/test.py
+++ scripts/test.py
@@ -13,15 +13,16 @@ from recon import *
 def main():
     '''
     Usage:
-        vis.py <blob_name>
+        vis.py <net_id> <blob_name>
     '''
     import docopt, textwrap
     main_args = docopt.docopt(textwrap.dedent(main.__doc__))
 
+    net_id = main_args['<net_id>']
     blob_name = main_args['<blob_name>']
 
-    rec = Reconstructor('caffenet_1000')
-    for i in range(96):
+    rec = Reconstructor(net_id)
+    for i in range(30):
         rec.canonical_image(blob_name, i, 5, '/tmp/test_{}.jpg'.format(i))
 
 
