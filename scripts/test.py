ef56d67d61eb73efba3be4ddba4fc9ff9e04a671
Michael Cogswell <cogswell@vt.edu>
2015-07-07 16:47:25 -0400

    add topk option and single reconstruction option to test.py

--- scripts/test.py
+++ scripts/test.py
@@ -2,28 +2,39 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+import skimage.io as io
+
 import caffe
 import caffe.proto.caffe_pb2 as cpb
 
 from cogswell import keyboard
 
-import recon.config as config
 from recon import *
+from recon.config import config
 
 def main():
     '''
     Usage:
-        vis.py <net_id> <blob_name>
+        vis.py (maxes|single) <net_id> <blob_name> [--topk K]
+    Options:
+        --topk K    How many images to display at once?
     '''
     import docopt, textwrap
     main_args = docopt.docopt(textwrap.dedent(main.__doc__))
 
     net_id = main_args['<net_id>']
     blob_name = main_args['<blob_name>']
+    topk = int(main_args['--topk'])
 
     rec = Reconstructor(net_id)
-    for i in range(30):
-        rec.canonical_image(blob_name, i, 5, '/tmp/test_{}.jpg'.format(i))
+    if main_args['maxes']:
+        for i in range(30):
+            rec.canonical_image(blob_name, i, topk, '/tmp/test_{}.jpg'.format(i))
+    elif main_args['single']:
+        imgs, recons = rec.reconstruct(blob_name)
+        for i, recon in enumerate(recons):
+            mult = config.nets[net_id].blob_multipliers[blob_name]
+            io.imsave('/tmp/recon_{}.jpg'.format(i), rec._showable(mult*recon))
 
 
 if __name__ == '__main__':
