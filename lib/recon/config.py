c76501dc900fe7c3370f1d14a798c54fcdbc71b9
Michael Cogswell <cogswell@vt.edu>
2015-07-11 21:04:26 -0400

    use GUIDED backprop (better for fc6/fc7) and refactor a bit

--- lib/recon/config.py
+++ lib/recon/config.py
@@ -5,65 +5,48 @@ from easydict import EasyDict as edict
 relu_backward_types = edict(dict(cpb.ReLUParameter.BackpropType.items()))
 relu_backward_types_inv = { v: k for k, v in relu_backward_types.iteritems()}
 
+net_config = {
+    'model_param': 'caffe/models/bvlc_reference_caffenet/bvlc_reference_caffenet.caffemodel',
+    'batch_size': 20,
+    'mean_fname': 'caffe/data/ilsvrc12/imagenet_mean.binaryproto',
+    'img_blob_name': 'data',
+    'data_layer_name': 'data',
+    'blob_multipliers': {
+        'conv1': 1.0,
+        'conv2': 4.0,
+        'conv3': 20.0,
+        'conv4': 40.0,
+        'conv5': 100.0,
+        'fc6': 400.0,
+        'fc7': 4096.0,
+    }
+}
+
 config = edict({
 'logger': {
     'dir': 'logs/',
     'name': 'parvis',
 },
-# 50 GB
-'lmdb_map_size': 50 * (1024**3),
+'lmdb_map_size': 1000 * (1024**3),
 'nets': {
-    'caffenet_1000': {
-        'relu_type': relu_backward_types.DECONV,
+    'caffenet_1000': dict({
+        'relu_type': relu_backward_types.GUIDED,
         'spec_wdata': 'specs/caffenet_1000_val.prototxt',
         'spec_nodata': 'specs/caffenet_1000_deploy.prototxt',
-        'model_param': 'caffe/models/bvlc_reference_caffenet/bvlc_reference_caffenet.caffemodel',
-        'batch_size': 20,
         'num_examples': 1000,
-        'mean_fname': 'caffe/data/ilsvrc12/imagenet_mean.binaryproto',
-        'img_blob_name': 'data',
-        'blob_multipliers': {
-            'conv1': 1.0,
-            'conv2': 2.0,
-            'conv3': 4.0,
-            'conv4': 8.0,
-            'conv5': 16.0,
-        }
-    },
-    'caffenet_imnet_val': {
+    }, **net_config),
+    'caffenet_imnet_val': dict({
         'relu_type': relu_backward_types.GUIDED,
         'spec_wdata': 'specs/caffenet_imnet_val.prototxt',
         'spec_nodata': 'specs/caffenet_deploy.prototxt',
-        'model_param': 'caffe/models/bvlc_reference_caffenet/bvlc_reference_caffenet.caffemodel',
-        'batch_size': 20,
         'num_examples': 50000,
-        'mean_fname': 'caffe/data/ilsvrc12/imagenet_mean.binaryproto',
-        'img_blob_name': 'data',
-        'blob_multipliers': {
-            'conv1': 1.0,
-            'conv2': 2.0,
-            'conv3': 4.0,
-            'conv4': 8.0,
-            'conv5': 16.0,
-        }
-    },
-    'caffenet_imnet_train': {
-        'relu_type': relu_backward_types.DECONV,
+    }, **net_config),
+    'caffenet_imnet_train': dict({
+        'relu_type': relu_backward_types.GUIDED,
         'spec_wdata': 'specs/caffenet_imnet_train.prototxt',
         'spec_nodata': 'specs/caffenet_deploy.prototxt',
-        'model_param': 'caffe/models/bvlc_reference_caffenet/bvlc_reference_caffenet.caffemodel',
-        'batch_size': 20,
         'num_examples': 1281167,
-        'mean_fname': 'caffe/data/ilsvrc12/imagenet_mean.binaryproto',
-        'img_blob_name': 'data',
-        'blob_multipliers': {
-            'conv1': 1.0,
-            'conv2': 2.0,
-            'conv3': 4.0,
-            'conv4': 8.0,
-            'conv5': 16.0,
-        }
-    }
+    }, **net_config),
 }
 }) 
 
