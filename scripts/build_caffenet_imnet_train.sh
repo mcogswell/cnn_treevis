6eb18de3e05f9badc90188fb436ab9d576acaee8
Michael Cogswell <cogswell@vt.edu>
2015-07-11 21:05:11 -0400

    generate dbs for fc6/fc7

--- scripts/build_caffenet_imnet_train.sh
+++ scripts/build_caffenet_imnet_train.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
-(scripts/build_db.py caffenet_imnet_train conv1 --gpu 13 --topk 16 &)
-(scripts/build_db.py caffenet_imnet_train conv2 --gpu 14 --topk 16 &)
-(scripts/build_db.py caffenet_imnet_train conv3 --gpu 15 --topk 16 &)
-(scripts/build_db.py caffenet_imnet_train conv4 --gpu 8 --topk 16 &)
-(scripts/build_db.py caffenet_imnet_train conv5 --gpu 9 --topk 16 &)
+(scripts/build_db.py caffenet_imnet_train conv1 --gpu 0 --topk 16 &)
+(scripts/build_db.py caffenet_imnet_train conv2 --gpu 1 --topk 16 &)
+(scripts/build_db.py caffenet_imnet_train conv3 --gpu 2 --topk 16 &)
+(scripts/build_db.py caffenet_imnet_train conv4 --gpu 3 --topk 16 &)
+(scripts/build_db.py caffenet_imnet_train conv5 --gpu 4 --topk 16 &)
+(scripts/build_db.py caffenet_imnet_train fc6 --gpu 2 --topk 16 &)
+(scripts/build_db.py caffenet_imnet_train fc7 --gpu 7 --topk 16 &)
--- scripts/build_caffenet_imnet_val.sh
+++ scripts/build_caffenet_imnet_val.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
-(scripts/build_db.py caffenet_imnet_val conv1 --gpu 0 &)
-(scripts/build_db.py caffenet_imnet_val conv2 --gpu 1 &)
-(scripts/build_db.py caffenet_imnet_val conv3 --gpu 2 &)
-(scripts/build_db.py caffenet_imnet_val conv4 --gpu 3 &)
-(scripts/build_db.py caffenet_imnet_val conv5 --gpu 4 &)
+(scripts/build_db.py caffenet_imnet_val conv1 --gpu 0 --topk 16 &)
+(scripts/build_db.py caffenet_imnet_val conv2 --gpu 1 --topk 16 &)
+(scripts/build_db.py caffenet_imnet_val conv3 --gpu 2 --topk 16 &)
+(scripts/build_db.py caffenet_imnet_val conv4 --gpu 3 --topk 16 &)
+(scripts/build_db.py caffenet_imnet_val conv5 --gpu 4 --topk 16 &)
+(scripts/build_db.py caffenet_imnet_val fc6 --gpu 5 --topk 16 &)
+(scripts/build_db.py caffenet_imnet_val fc7 --gpu 6 --topk 16 &)
