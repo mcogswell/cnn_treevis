b18705d6e6c7cea1d18a6057837419e67273c333
Michael Cogswell <cogswell@vt.edu>
2015-10-15 20:35:34 -0400

    add gallery of images

--- app.py
+++ app.py
@@ -20,15 +20,19 @@ from recon.config import config
 
 @app.route('/imgs/feat/<path:path>')
 def img_feat(path):
-    return send_file(pth.join('data/feat/{}'.format(path)))
+    return send_file(pth.join('data/feat/', path))
 
-'''
-@app.route('/imgs/recon/<path:path>')
-def img_recon(path):
-    # TODO
-    return send_file(pth.join('data/recon/{}'.format(path)))
-'''
 
+@app.route('/imgs/gallery/<path:path>')
+def img_gallery(path):
+    return send_file(pth.join('data/gallery/', path))
+
+
+@app.route('/gallery')
+def gallery():
+    img_fnames = [pth.basename(fname) for fname in glob.glob('data/gallery/*')]
+    # TODO: better/parameterized defaults
+    return render_template('gallery.html', img_fnames=img_fnames, imgs_per_row=5, blob_name='conv5', act_id=4)
 
 
 @app.route('/vis')
@@ -38,19 +42,19 @@ def vis_activation():
     return render_template('vis.html', blob_name=blob_name, act_id=act_id)
 
 
-@app.route('/vis/tree.json')
+@app.route('/vis/tree/get')
 def json_tree():
     blob_name = request.args.get('blob_name', '')
     act_id = int(request.args.get('act_id', ''))
     # should input layer_name, not blob_name
-    tree = vis_tree.tree(blob_name, act_id)
+    tree = get_vis_tree(net_id).tree(blob_name, act_id)
     return jsonify(tree)
 
 
 @app.route('/vis/tree/maxes')
 def json_tree_maxes():
     blob_name = request.args.get('blob_name', '')
-    maxes = vis_tree.max_idxs(blob_name)
+    maxes = get_vis_tree(net_id).max_idxs(blob_name)
     return jsonify(maxes=maxes)
 
 
@@ -59,9 +63,19 @@ def json_tree_expand():
     blob_name = request.args.get('blob_name', '')
     act_id = int(request.args.get('act_id', ''))
     # should input layer_name, not blob_name
-    children = vis_tree.expand(blob_name, act_id)
+    children = get_vis_tree(net_id).expand(blob_name, act_id)
     return jsonify(children=children)
 
+
+
+_vis_trees = {}
+def get_vis_tree(img_fname):
+    if img_fname in _vis_trees:
+        return _vis_trees[img_fname]
+    else:
+        vis_tree = VisTree(net_id)
+        _vis_trees[img_fname] = vis_tree
+        return vis_tree
 
 
 
@@ -73,12 +87,11 @@ def main():
     Options:
         --debug     Launch the app in debug mode? [default: false]
     '''
-    global rec, vis_tree
+    global net_id
     import docopt, textwrap
     main_args = docopt.docopt(textwrap.dedent(main.__doc__))
+    net_id = main_args['<net_id>']
 
-    rec = Reconstructor(main_args['<net_id>'])
-    vis_tree = VisTree(main_args['<net_id>'])
     app.run(host='fukushima.ece.vt.edu', debug=main_args['--debug'])
 
 
